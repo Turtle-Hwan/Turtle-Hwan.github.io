c79a0190bb6c8649eab95a2628742947cbf32b94
Kim Ji-hwan <kjhwan0802@naver.com>
2022-09-18 14:10:21 +0000

    Init : gatsby-starter-bee 이용해서 블로그 만들기
    Related to
    https://github.com/JaeYeopHan/gatsby-starter-bee

---
+++ my-blog-starter/gatsby-meta-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: `Turtle-Hwan`,
+  description: `Blog posted about ...`,
+  author: `[김지환]`,
+  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  siteUrl: `https://turtle-hwan.github.io/`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: ``, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'Turtle-Hwan/Turtle-Hwan.github.io', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+  ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
+
+}
---
+++ my-blog-starter/src/components/footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './index.scss'
+
+export const Footer = () => (
+  <footer className="footer">
+    ©2022 <a href="https://github.com/Turtle-Hwan/Turtle-Hwan.github.io">Turtle-Hwan</a> <br/> Based on{' '}
+    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
+      Gatsby-starter-bee
+    </a> by <a href="https://github.com/JaeYeopHan">Jbee</a>
+  </footer>
+)
