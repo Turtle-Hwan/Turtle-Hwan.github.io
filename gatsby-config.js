df9ee677a545e847040c83e36f844a2984185d18
Kim jihwan <kjhwan0802@naver.com>
2023-06-21 15:50:36 +0900

    INIT : 'src' 폴더 구조 변경 / typescript 초기 환경 구성(tsconfig.json) / gatsby-config.js 에 plugin 추가 / gatsby-node.js 에 webpack config 추가
    - tsconfig.json에 paths 옵션에서 절대경로를 사용하도록 매핑했으므로, gatsby-node.js에 webpack config를 추가해야 한다.

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,14 +15,22 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
-    `gatsby-plugin-image`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-typescript',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        isTSX: true,
+        allExtensions: true,
       },
     },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
