5af0c4df0d5597327dbcb6ccee137ac19bf6cb81
Kim jihwan <kjhwan0802@naver.com>
2023-06-21 20:44:44 +0900

    emotionJS 설치 / gatsby Link API 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
         allExtensions: true,
       },
     },
+    `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     // {
--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,15 +1,42 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
-import Text from 'components/Text'
 
-type InfoPageProps = {}
+type InfoPageProps = {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+        description: string
+        author: string
+      }
+    }
+  }
+}
 
-const InfoPage: FunctionComponent<InfoPageProps> = function () {
+const InfoPage: FunctionComponent<InfoPageProps> = function ({
+  data: {
+    site: {
+      siteMetadata: { title, description, author },
+    },
+  },
+}) {
   return (
     <div>
-      <Text text="Hello" />
+      {title} {description} {author}
     </div>
   )
 }
 
 export default InfoPage
+
+export const metadataQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`
