4ae9f1750f622748a2d3463bbe9c65c783136031
Kim jihwan <kjhwan0802@naver.com>
2023-06-23 16:08:49 +0900

    emotionJS test

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
+import styled from '@emotion/styled'
+import { Global, css } from '@emotion/react'
 
 type InfoPageProps = {
   data: {
@@ -13,6 +15,34 @@ type InfoPageProps = {
   }
 }
 
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-size: 20px;
+  }
+`
+
+const TextStyle = css`
+  font-size: 18px;
+  font-weight: 700;
+  color: gray;
+`
+
+const Text1 = styled.div<{ disable: boolean }>`
+  font-size: 20px;
+  font-weight: 700;
+  text-decoration: ${({ disable }) => (disable ? 'line-through' : 'none')};
+`
+
+const Text2 = styled('div')<{ disable: boolean }>(({ disable }) => ({
+  fontSize: '15px',
+  color: 'blue',
+  textDecoration: disable ? 'line-through' : 'none',
+}))
+
 const InfoPage: FunctionComponent<InfoPageProps> = function ({
   data: {
     site: {
@@ -22,7 +52,10 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
 }) {
   return (
     <div>
-      {title} {description} {author}
+      <Global styles={globalStyle} />
+      <div css={TextStyle}>{title}</div>
+      <Text1 disable={true}>{description}</Text1>
+      <Text2 disable={true}>{author}</Text2>
     </div>
   )
 }
